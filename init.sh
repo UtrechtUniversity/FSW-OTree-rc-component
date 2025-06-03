0d8d1c6c93669f163835e365783a29b2faa50d25
Sprui101 <A.G.L.Spruit@uu.nl>
2025-06-03 17:06:52 +0200

    Second attempt at web server configuration in Research Cloud

--- init.sh
+++ init.sh
@@ -10,15 +10,15 @@ nginx_conf="/etc/nginx/conf.d/ssl_main.conf"
 
 # Define two variables containing the correct reverse proxy configuration for
 # the two Docker containers
-otree_conf="location \/ {\n     \tproxy_pass http:\/\/otree:8000\/;\n     \tproxy_set_header X-Forwarded-Proto $scheme;\n     \tproxy_set_header X-Forwarded-Port $server_port;\n     \tproxy_set_header X-Real-IP $remote_addr;\n     \tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n     \tproxy_set_header X-Forwarded-Host $server_name;\n     \tproxy_set_header Host $host;\n     \tproxy_set_header Upgrade $http_upgrade;\n     \tproxy_set_header Connection $connection_upgrade;\n}"
-locust_conf="location \/locust\/ {\n     \tproxy_pass http:\/\/locust:8089\/;\n     \tproxy_set_header X-Forwarded-Proto $scheme;\n     \tproxy_set_header X-Forwarded-Port $server_port;\n     \tproxy_set_header X-Real-IP $remote_addr;\n     \tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n     \tproxy_set_header X-Forwarded-Host $server_name;\n     \tproxy_set_header Host $host;\n     \tproxy_set_header Upgrade $http_upgrade;\n     \tproxy_set_header Connection $connection_upgrade;\n}"
+#otree_conf="location \/ {\n     \tproxy_pass http:\/\/otree:8000\/;\n     \tproxy_set_header X-Forwarded-Proto $scheme;\n     \tproxy_set_header X-Forwarded-Port $server_port;\n     \tproxy_set_header X-Real-IP $remote_addr;\n     \tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n     \tproxy_set_header X-Forwarded-Host $server_name;\n     \tproxy_set_header Host $host;\n     \tproxy_set_header Upgrade $http_upgrade;\n     \tproxy_set_header Connection $connection_upgrade;\n}"
+#locust_conf="location \/locust\/ {\n     \tproxy_pass http:\/\/locust:8089\/;\n     \tproxy_set_header X-Forwarded-Proto $scheme;\n     \tproxy_set_header X-Forwarded-Port $server_port;\n     \tproxy_set_header X-Real-IP $remote_addr;\n     \tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n     \tproxy_set_header X-Forwarded-Host $server_name;\n     \tproxy_set_header Host $host;\n     \tproxy_set_header Upgrade $http_upgrade;\n     \tproxy_set_header Connection $connection_upgrade;\n}"
 
 # Let Nginx serve the Docker containers running on port 8000, 8089 respectively
 # instead of the regular static web page 'index.html'
 
 # Note the use of double quotes around the sed expression. This is crucial
 # It allows for environment variable substitution
-sed -i "s|root /var/www/html;|$locust_conf$otree_conf|" "$nginx_conf"
+sed -i "s|root /var/www/html;|location \/ {\n     \tproxy_pass http:\/\/localhost:8000\/;\n     \tproxy_set_header X-Forwarded-Proto $scheme;\n     \tproxy_set_header X-Forwarded-Port $server_port;\n     \tproxy_set_header X-Real-IP $remote_addr;\n     \tproxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;\n     \tproxy_set_header X-Forwarded-Host $server_name;\n     \tproxy_set_header Host $host;\n     \tproxy_set_header Upgrade $http_upgrade;\n     \tproxy_set_header Connection $connection_upgrade;\n}|" "$nginx_conf"
 sed -i "s|index index.html index.htm;||" "$nginx_conf"
 
 # Restart nginx to reload configuration
